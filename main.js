411861e858fc5cf14adfd4126d160cedfb781214
mmamorim <marc.m.amorim@gmail.com>
2024-03-13 20:51:50 -0300

    v1

--- main.js
+++ main.js
@@ -3,14 +3,24 @@ import imageLab from "./imageLab.js"
 console.log('imageLab', imageLab);
 imageLab.setLoadButtonID('btnLoad')
 
-document.getElementById('btnFilter01').addEventListener('click', filtro);
+document.getElementById('btn1').addEventListener('click', filtro);
+document.getElementById('btn1').addEventListener('click', filtro2);
 
 function filtro() {
     for(let y=1; y <= imageLab.image1.height; y++) {
         for(let x=1; x <= imageLab.image1.width; x++) {
-            let pixel = imageLab.image1.getPixel(x,y)
             // processamento de cada pixel da imagem
+            let pixel = imageLab.image1.getPixel(x,y)
+            let brilho = 3.5
+            pixel.r = pixel.r * brilho
+            pixel.g = pixel.g * brilho
+            pixel.b = pixel.b * brilho
+            imageLab.image2.setPixel(x,y,pixel)
         }
     }
     imageLab.image2.refresh()
+}
+
+function filtro2() {
+
 }
