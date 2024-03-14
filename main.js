1c377c78bf9216532bce5490939079a78247ba75
mmamorim <marc.m.amorim@gmail.com>
2024-03-13 21:24:15 -0300

    v2

--- main.js
+++ main.js
@@ -3,15 +3,16 @@ import imageLab from "./imageLab.js"
 console.log('imageLab', imageLab);
 imageLab.setLoadButtonID('btnLoad')
 
-document.getElementById('btn1').addEventListener('click', filtro);
-document.getElementById('btn1').addEventListener('click', filtro2);
+document.getElementById('btn1').addEventListener('click', brilho);
+document.getElementById('btn2').addEventListener('click', tonsCinza);
+document.getElementById('btn3').addEventListener('click', inverte);
 
-function filtro() {
+function brilho() {
     for(let y=1; y <= imageLab.image1.height; y++) {
         for(let x=1; x <= imageLab.image1.width; x++) {
             // processamento de cada pixel da imagem
             let pixel = imageLab.image1.getPixel(x,y)
-            let brilho = 3.5
+            let brilho = 2.5
             pixel.r = pixel.r * brilho
             pixel.g = pixel.g * brilho
             pixel.b = pixel.b * brilho
@@ -21,6 +22,31 @@ function filtro() {
     imageLab.image2.refresh()
 }
 
-function filtro2() {
+function tonsCinza() {
+    for(let y=1; y <= imageLab.image1.height; y++) {
+        for(let x=1; x <= imageLab.image1.width; x++) {
+            // processamento de cada pixel da imagem
+            let pixel = imageLab.image1.getPixel(x,y)
+            let media = (pixel.r+pixel.g+pixel.b)/3
+            pixel.r = media
+            pixel.g = media
+            pixel.b = media
+            imageLab.image2.setPixel(x,y,pixel)
+        }
+    }
+    imageLab.image2.refresh()
+}
 
+function inverte() {
+    for(let y=1; y <= imageLab.image1.height; y++) {
+        for(let x=1; x <= imageLab.image1.width; x++) {
+            // processamento de cada pixel da imagem
+            let pixel = imageLab.image1.getPixel(x,y)
+            pixel.r = 255-pixel.r
+            pixel.g = 255-pixel.g
+            pixel.b = 255-pixel.b            
+            imageLab.image2.setPixel(x,y,pixel)
+        }
+    }
+    imageLab.image2.refresh()
 }
