81c778202f21ad9933d5c67dc7b8900e00a71f42
mmamorim <marc.m.amorim@gmail.com>
2024-03-20 20:30:39 -0300

    v5

--- filtros/flipH.js
+++ filtros/flipH.js
@@ -1,10 +1,13 @@
 function flipH(imageLab) {
+    let meio = imageLab.image1.width / 2
+    let w = imageLab.image1.width
     for(let y=1; y <= imageLab.image1.height; y++) {
-        for(let x=1; x <= imageLab.image1.width; x++) {
+        for(let x=1; x <= meio; x++) {
             // processamento de cada pixel da imagem
-            let pixel = imageLab.image1.getPixel(x,y)
-            
-            //imageLab.image2.setPixel(x,y,pixel)
+            let pixel1 = imageLab.image1.getPixel(x,y)
+            let pixel2 = imageLab.image1.getPixel(w-x+1,y)
+            imageLab.image2.setPixel(w-x+1,y,pixel1)
+            imageLab.image2.setPixel(x,y,pixel2)
         }
     }
     imageLab.image2.refresh()
