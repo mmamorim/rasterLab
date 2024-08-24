320136d3d78668bc8e7f40abcd45fbe3c9829768
mmamorim <marc.m.amorim@gmail.com>
2024-08-23 21:41:06 -0300

    242-v2

--- filtros/monocromatico.js
+++ filtros/monocromatico.js
@@ -1,16 +1,16 @@
-function brilho(imageLab) {
+function mono(imageLab) {
     for(let y=1; y <= imageLab.image1.height; y++) {
         for(let x=1; x <= imageLab.image1.width; x++) {
             // processamento de cada pixel da imagem
             let pixel = imageLab.image1.getPixel(x,y)
-            let brilho = 2
-            pixel.r = pixel.r * brilho
-            pixel.g = pixel.g * brilho
-            pixel.b = pixel.b * brilho
+            let media = parseInt((pixel.r + pixel.g + pixel.b) / 3)
+            pixel.r = media
+            pixel.g = media
+            pixel.b = media
             imageLab.image2.setPixel(x,y,pixel)
         }
     }
-    imageLab.image2.refresh()
+    imageLab.image2.refresh()    
 }
 
-export default brilho
+export default mono
--- main.js
+++ main.js
@@ -2,11 +2,15 @@ import imageLab from "./imageLab/imageLab.js"
 import createFilters from './imageLab/createFilters.js'
 
 import brilho from "./filtros/brilho.js"
+import mono from "./filtros/monocromatico.js"
+import sepia from "./filtros/sepia.js"
 
 console.log('imageLab', imageLab);
 imageLab.setLoadButtonID('btnLoad')
 
 createFilters(imageLab, [
     { label: 'Brilho', method: brilho },
+    { label: 'Monocromático', method: mono },
+    { label: 'Sépia', method: sepia },
 ])
 
